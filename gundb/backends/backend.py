2cde5b8fe0fd3763996b6b459b61c393a6101e89
Omar Elawady <omarelawady1998@gmail.com>
2019-07-18 15:55:25 +0200

    Code minimization

--- gundb/backends/backend.py
+++ gundb/backends/backend.py
@@ -1,6 +1,11 @@
 import json
+import copy
 from ..consts import *
 from .resolvers import *
+def write(data):
+    fd = open('out.txt')
+def uniquify(lst):
+    return lst
 
 class BackendMixin:
     def get_object_by_id(self, obj_id, schema=None):
@@ -17,99 +22,36 @@ class BackendMixin:
             self.db[soul] = {METADATA:{STATE:{}}}
         self.db[soul][key] = value
         self.db[soul][METADATA][STATE][key] = state
-
+        
         if isinstance(value, str):
             try:
                 value = json.loads(value)
             except:
-                pass
+                pass 
 
-        rootobjects = list(filter_root_objects(graph))
-        # find its parent to get
-        def do(soul, key, value, graph):
-            obj = None
-            if is_root_soul(soul):
-                schema, obj_id = parse_schema_and_id(soul)
-                obj = self.get_object_by_id(obj_id, schema)
-                obj = self.set_object_attr(obj, 'id', obj_id)
-                # print("object update setting attr {} with value {}".format(key, resolve_v(value, graph)))
+        if is_root_soul(soul): # root object
+            root = soul
+            path = [key]
+        else: 
+            path = search(soul, graph)
+            if not path:
+                print("soul: {} Not found\n\n".format(soul))
+                return 0
+            root = path[0]
+            path = path[1:] + [key]
 
-                if key.startswith("list_"):
-                    theattr = key
-                    resolved_list = resolve_v(value, graph)
-                    def hash_dict(adict):
-                        return str(adict)
-
-                    d_as_list = resolved_list.values()
-                    unique_items = []
-                    added = set()
-                    for k, v in resolved_list.items():
-                        if str(v) not in added:
-                            added.add(str(v))
-                            unique_items.append(v)
-
-                    thelist = unique_items
-                    try:
-                        obj = self.set_object_attr(obj, theattr, thelist)
-                    except Exception as e:
-                        print(e)
-                else:
-                    obj = self.set_object_attr(obj, key, resolve_v(value, graph))
-                print(obj)
-                print("saved!!!")
-                self.save_object(obj, obj_id, schema)
-                return obj
-            else:
-                objpath = path = search(soul, graph, rootobjects)
-                if not objpath:
-                    # FIXME doesn't work 
-
-                    """
-                        put bcdb => soul jxvd6ufqyCFeQ9mtcpwJ key jxvd6ufkPTDohIx value white state 1562649540756
-                        {
-                            "jxvd6ufqyCFeQ9mtcpwJ": {
-                                "_": {
-                                    "#": "jxvd6ufqyCFeQ9mtcpwJ",
-                                    ">": {
-                                        "jxvd6ufkPTDohIx": 1562649540756
-                                    }
-                                },
-                                "jxvd6ufkPTDohIx": "white"
-                            }
-                        }
-                        [---]can't find : jxvd6ufqyCFeQ9mtcpwJ jxvd6ufkPTDohIx white
-                    """
-                    print("[---]can't find :", soul, key, value) 
-                    return
-                objcontent = path + [{"#":soul}, graph]
-
-                schema, obj_id = parse_schema_and_id(objpath[0])
-                if not schema:
-                    return 
-                # print("*****schema:", schema)
-                objdata = do(*objcontent)
-
-                # print(objpath)
-
-                objinfo = objpath[0]
-                objpath = objpath[1:]
-
-                obj = self.get_object_by_id(obj_id, schema)
-                
-                # print("objpath: ", objpath)
-                while objpath:
-                    attr = objpath.pop(0)
-                    try:
-                        obj = getattr(obj, attr)
-                    except:
-                        return
-                obj = objdata
-                self.save_object(obj, schema)
-                print("success \n {}".format(obj))
-
-        do(soul, key, value, graph)
-        graph[soul][key] = value
-
+        value = resolve_v(value, graph)
+        if key.startswith('list_'):
+            value = uniquify(value.values())
+        
+        schema, index = parse_schema_and_id(root)
+        root_object = self.get_object_by_id(index, schema)
+        current = root_object
+        for e in path[1:-1]:
+            current = getattr(current, e)
+        
+        current[path[-1]] = value
+        self.save_object(root_object, index, schema)
 
     def get(self, soul, key=None):
         ret = {SOUL: soul, METADATA:{SOUL:soul, STATE:{}}}
