e24c1ddcd183be5f6255bfc0f7abeb650d71c5ef
Omar Elawady <omarelawady1998@gmail.com>
2019-08-04 11:07:50 +0200

    Recieve and return defaultdicts

--- gundb/backends/rediskv.py
+++ gundb/backends/rediskv.py
@@ -1,8 +1,11 @@
 from collections import defaultdict
 from .backend import BackendMixin
-from .utils import defaultify
+from .utils import defaultify, fix_lists
+from ..consts import METADATA, SOUL, STATE
 import json
-
+ignore = '_'
+LISTDATA = 'L'
+MAPPING  = '[]'
 
 def format_object_id(schema, id):
     return "{}://{}".format(schema, id)
@@ -16,9 +19,9 @@ class RedisKV(BackendMixin):
     def get_object_by_id(self, obj_id, schema=None):
         full_id = format_object_id(schema, obj_id)
         if self.redis.exists(full_id):
-            return json.loads(self.redis.get(full_id))
+            return defaultify(json.loads(self.redis.get(full_id)))
         else:
-            return {'id':obj_id}
+            return defaultify({'id':obj_id})
 
     def set_object_attr(self, obj, attr, val):
         obj[attr] = val
@@ -26,8 +29,35 @@ class RedisKV(BackendMixin):
 
     def save_object(self, obj, obj_id, schema=None):
         full_id = format_object_id(schema, obj_id)
+        obj = self.delegate_list_metadatata(obj)
+
         self.redis.set(full_id, json.dumps(obj))
 
+    def delegate_list_metadatata(self, obj):
+        if not isinstance(obj, dict):
+            return obj
+        for k, v in obj.items():
+            if k == METADATA:
+                continue
+            if k.startswith("list_"):
+                obj[k] = self.delegate_list_metadatata(v)
+                obj[METADATA][LISTDATA][k][METADATA] = v[METADATA]
+                mapping, result_list = self.extract_mapping_list(obj[k])
+                obj[METADATA][LISTDATA][k][MAPPING] = mapping
+                obj[k] = result_list
+            else:
+                obj[k] = self.delegate_list_metadatata(v)
+        return obj
+
+    def extract_mapping_list(self, list_obj):
+        mapping = {}
+        result = []
+        del list_obj[METADATA]
+        for i, k in enumerate(list_obj.keys()):
+            mapping[k] = i
+            result.append(list_obj[k])
+        return mapping, result
+     
     def __setitem__(self, k, v):
         self.db[k] = v
 
