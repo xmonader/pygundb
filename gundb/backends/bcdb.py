dbea6f2efa0e2a56fb1dbd86ee335c0a5de06c28
Omar Elawady <omarelawady1998@gmail.com>
2019-08-20 11:14:06 +0200

    Ignore unrecoverable objects

--- gundb/backends/bcdb.py
+++ gundb/backends/bcdb.py
@@ -1,6 +1,6 @@
 import re
 import json
-from ..consts import *
+from ..consts import STATE, LISTDATA, MAPPING, SOUL, METADATA
 from .backend import BackendMixin
 from .utils import defaultify
 from .resolvers import desolve
@@ -8,64 +8,57 @@ import Jumpscale
 from Jumpscale import j
 from collections import defaultdict
 
-BCDBMETADATA = 'metadata'
+BCDBMETADATA = "metadata"
 SCHEME_UID_PAT = "(?P<schema>.+?)://(?P<id>.+)"
 
-schemas = ["""
+schemas = [
+    """
 @url = proj.todoitem
 a= "" (S)
 title* = "" (S)
 done = False (B)
 """,
-
-"""
+    """
 @url = proj.todolist
 a= "" (S)
 name* = "" (S)
 list_todos = (LO) !proj.todoitem
-"""
-,
-"""
+""",
+    """
 @url = proj.simple
 metadata = "" (S)
 attr1* = "" (S)
 attr2 = 0 (I)
 list_mychars = (LS) 
-"""
-,
-"""
+""",
+    """
 @url = proj.email
 metadata = "" (S)
 addr* = "" (S)
-"""
-,
-"""
+""",
+    """
 @url = proj.person
 metadata = "" (S)
 name* = "" (S)
 email = (O) !proj.email
-"""
-,
-"""
+""",
+    """
 @url = proj.os
 metadata = "" (S)
 name* = "" (S)
-"""
-,
-"""
+""",
+    """
 @url = proj.phone
 metadata = "" (S)
 model* = "" (S)
 os = (O) !proj.os
-"""
-,
-"""
+""",
+    """
 @url = proj.lang
 metadata = "" (S)
 name* = ""
-"""
-,
-"""
+""",
+    """
 @url = proj.human
 metadata = "" (S)
 name* = "" (S)
@@ -73,22 +66,22 @@ list_favcolors = (LS)
 list_langs = (LO) !proj.lang
 phone = (O) !proj.phone
 """,
-"""
+    """
 @url = proj.post
 metadata = "" (S)
 name = "" (S)
 title* = "" (S)
 body = "" (S)
-"""
-,
-"""
+""",
+    """
 @url = proj.blog
 metadata = "" (S)
 name* = "" (S)
 list_posts = (LO) !proj.post
 headline = "" (S)
-"""
+""",
 ]
+
 
 def parse_schema_and_id(s):
     m = re.match(SCHEME_UID_PAT, s)
@@ -112,6 +105,7 @@ class BCDB(BackendMixin):
             m = self.bcdb.model_get_from_schema(s)
             # o = m.new()
             # o.save()
+
     def get_schema_by_url(self, url):
         schema = j.data.schema.get_from_url_latest(url=url)
         return schema
@@ -125,9 +119,9 @@ class BCDB(BackendMixin):
         try:
             return m.get(obj_id=obj_id)
         except:
-            #import ipdb; ipdb.set_trace()
-            o = m.new({'id':int(obj_id)})
-            #o = m.new()
+            # import ipdb; ipdb.set_trace()
+            o = m.new({"id": int(obj_id)})
+            # o = m.new()
             o._model = m
 
             o.save()
@@ -138,13 +132,16 @@ class BCDB(BackendMixin):
         graph = {}
         for root_obj in root_objs:
             soul = json.loads(getattr(root_obj, BCDBMETADATA))[SOUL]
-            graph[soul] = self.recover_obj(root_obj)
+            try:
+                graph[soul] = self.recover_obj(root_obj)
+            except:
+                pass
         return desolve(graph)
 
     def recover_obj(self, root_obj):
         db_form = defaultify(root_obj._ddict)
         return self.convert_to_graph(self.parse_metadata(db_form))
-    
+
     def parse_metadata(self, obj):
         if not isinstance(obj, dict):
             return obj
@@ -157,7 +154,7 @@ class BCDB(BackendMixin):
             else:
                 result[k] = self.parse_metadata(v)
         return result
-    
+
     def stringify_metadata(self, obj):
         if not isinstance(obj, dict):
             return obj
@@ -175,7 +172,6 @@ class BCDB(BackendMixin):
         setattr(obj, attr, val)
         return obj
 
-
     def save_object(self, obj, obj_id, schema=None):
         db_obj = self.get_object_by_id(obj_id, schema)
         obj = self.stringify_metadata(self.delegate_list_metadatata(obj))
@@ -189,7 +185,6 @@ class BCDB(BackendMixin):
                 self.set_graph_to_obj(v, getattr(obj, k))
             else:
                 setattr(obj, k, v)
-
 
     def __setitem__(self, k, v):
         self.db[k] = v
--- gundb/backends/mongo.py
+++ gundb/backends/mongo.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from .backend import BackendMixin
 from .utils import defaultify
 from .resolvers import desolve
+from ..consts import METADATA
 
 
 class cuteobj:
@@ -14,9 +15,11 @@ class cuteobj:
     def __str__(self):
         return "cuteobj: {} ".format(str(self.__dict__))
 
+
 class Mongo(BackendMixin):
-    def __init__(self, connstring='mongodb://localhost:27017'):
+    def __init__(self, connstring="mongodb://localhost:27017"):
         from pymongo import MongoClient
+
         self.cl = MongoClient(connstring)
         self.mongodb = self.cl.test_database
         self.db = defaultdict(lambda: defaultdict(lambda: defaultdict()))
@@ -28,7 +31,7 @@ class Mongo(BackendMixin):
         if not obj:
             col.insert_one({"id": obj_id})
             obj = col.find_one({"id": obj_id})
-        del obj['_id']
+        del obj["_id"]
         return defaultify(obj)
 
     def set_object_attr(self, obj, attr, val):
@@ -38,22 +41,18 @@ class Mongo(BackendMixin):
     def save_object(self, obj, obj_id, schema=None):
         col = self.mongodb[schema]
         obj = self.delegate_list_metadatata(obj)
-        col.find_one_and_update({"id":obj_id}, {"$set": obj}, upsert=True)
+        col.find_one_and_update({"id": obj_id}, {"$set": obj}, upsert=True)
 
     def recover_graph(self):
         schmeas = self.mongodb.list_collection_names()
         graph = {}
         for schema in schmeas:
             for obj in self.mongodb[schema].find({}):
-                full_schema_id = schema + "://" + str(obj['id'])
-                del obj['_id']
-                graph[full_schema_id] = self.convert_to_graph(obj)
+                full_schema_id = schema + "://" + str(obj["id"])
+                del obj["_id"]
+                if METADATA in obj:
+                    graph[full_schema_id] = self.convert_to_graph(obj)
         return desolve(graph)
-
-    def recover_obj(self, key):
-        db_form = defaultify(json.loads(self.redis.get(key)))
-        return self.convert_to_graph(db_form)
-    
 
     def __setitem__(self, k, v):
         self.db[k] = v
