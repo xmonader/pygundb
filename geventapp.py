7e3871e1d7fb7e8aaac2eb1f8087ce6f7e1808f0
Omar Elawady <omarelawady1998@gmail.com>
2019-08-21 10:53:23 +0200

    Inject handler

--- geventapp.py
+++ geventapp.py
@@ -1,5 +1,21 @@
-from gundb.geventserver import geventserverapp
+import sys
+
+from gundb.geventserver import GeventGunServer
+from gundb.gunrequesthandler import GUNRequestHandler
+from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
+from collections import OrderedDict
+from functools import partialmethod
+
+
+def build_gun_server(handler):
+    class GeventGunServerWithHandler(GeventGunServer):
+        def __init__(self, *args, **kwargs):
+            super().__init__(handler, *args, **kwargs)
+
+    return GeventGunServerWithHandler
 
 
 if __name__ == "__main__":
+    handler = GUNRequestHandler(sys.argv[1])
+    geventserverapp = WebSocketServer(("", 8000), Resource(OrderedDict([("/", build_gun_server(handler))])))
     geventserverapp.serve_forever()
