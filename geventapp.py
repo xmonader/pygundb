95ad50bf5a5878c394e6e40573dee2d37afd8873
Omar Elawady <omarelawady1998@gmail.com>
2019-08-21 12:22:57 +0200

    Pass handler

--- geventapp.py
+++ geventapp.py
@@ -1,6 +1,6 @@
 import sys
 
-from gundb.geventserver import build_gun_server
+from gundb.geventserver import AppRunner, GeventGunServer
 from gundb.gunrequesthandler import GUNRequestHandler
 from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
 from collections import OrderedDict
@@ -8,5 +8,5 @@ from functools import partialmethod
 
 if __name__ == "__main__":
     handler = GUNRequestHandler(sys.argv[1])
-    geventserverapp = WebSocketServer(("", 8000), Resource(OrderedDict([("/", build_gun_server(handler))])))
+    geventserverapp = WebSocketServer(("", 8000), AppRunner(GeventGunServer, handler))
     geventserverapp.serve_forever()
--- gundb/geventserver.py
+++ gundb/geventserver.py
@@ -3,24 +3,41 @@ from collections import OrderedDict
 import json
 
 
-def build_gun_server(handler):
-    class GeventGunServer(WebSocketApplication):
-        def on_open(self):
-            print("Got client connection")
-            handler.add_peer(self.ws)
+class GeventGunServer(WebSocketApplication):
+    def __init__(self, ws, handler):
+        super().__init__(ws)
+        self.handler = handler
 
-        def on_message(self, message):
-            resp = {"ok": True}
-            if message is not None:
-                handler.process_message(message)
+    def on_open(self):
+        print("Got client connection")
+        self.handler.add_peer(self.ws)
 
-        def on_close(self, reason):
-            handler.remove_peer(self.ws)
-            print("Peers now: {}".format(handler.peers))
-            print(reason)
+    def on_message(self, message):
+        resp = {"ok": True}
+        if message is not None:
+            self.handler.process_message(message)
 
-        def sendall(self, resp):
-            for client in self.ws.handler.server.clients.values():
-                client.ws.send(json.dumps(resp))
+    def on_close(self, reason):
+        self.handler.remove_peer(self.ws)
+        print("Peers now: {}".format(self.handler.peers))
+        print(reason)
 
-    return GeventGunServer
+    def sendall(self, resp):
+        for client in self.ws.handler.server.clients.values():
+            client.ws.send(json.dumps(resp))
+
+
+class AppRunner:
+    def __init__(self, app, handler):
+        self.app = app
+        self.handler = handler
+
+    def __call__(self, environ, start_response):
+        is_websocket_call = "wsgi.websocket" in environ
+        current_app = self.app
+
+        if is_websocket_call:
+            ws = environ["wsgi.websocket"]
+            current_app = current_app(ws, self.handler)
+            current_app.handle()
+        return []
