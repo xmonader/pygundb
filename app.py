f117ab3c5177d307fbb3a4a8baa628729f842c23
xmonader <xmonader@gmail.com>
2019-09-08 20:25:10 +0200

    use GUNDB varname in makefile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from gundb.test import app
+from gundb.server import app
 from gundb.gunrequesthandler import GUNRequestHandler
+
 def build_app(backend):
     app.config["handler"] = GUNRequestHandler(backend)
     return app
